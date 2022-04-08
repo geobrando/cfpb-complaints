b45e90fa366d4428fddceb122a68d5c8418e0f70
geobrando <geobrando@mail.com>
2022-04-07 22:00:15 -0400

    moar organized

--- download_data.R
+++ download_data.R
@@ -1,23 +1,38 @@
 
 
-data.path <- file.path(".","Data")
-
-#Set the url for the data
-url <- "https://files.consumerfinance.gov/ccdb/complaints.csv.zip"
-
-#For data dictionary, go here:
-#https://cfpb.github.io/api/ccdb/fields.html
-
-#Check if the data directory exists and create it if it doesn't
-if(!dir.exists(data.path)){
-  dir.create(data.path)
+download_unzip_data <- function(url,dest=NULL) {
+  # 1) Downloads a zip file from a url to a temp file, 
+  # 2) Unzips to a "./data/raw" folder by default
+  # 3) returns list with paths to unzipped file(s)
+  
+  if(is.null(dest)) {dest <- file.path(".","data","raw")}
+  
+  tryCatch(
+    {
+      temp <-tempfile()
+      message("Attempting to download data...")
+      download.file(data.url,destfile = temp )
+      message("Unzipping data file...")
+      files <- unzip(zipfile = temp,list=TRUE)
+      unzip(zipfile = temp,exdir = dest)
+      unlink(temp)
+    },
+    error=function(cond) {
+      message("Error message when attempting to download and unzip file:")
+      message(cond)
+    },
+    warning=function(cond) {
+      message("Warning message when attempting to download and unzip file:")
+      message(cond)
+    },
+    finally={
+      message(paste("Successfully downloaded file from",url,"and unzipped to",
+                    dest))
+      return(files)
+    }
+  )
 }
 
-data.file <- file.path(data.path, basename(url))
-
-#Download the data to the data folder
-download.file(url, destfile = data.file)
 
 
-#Unzip everything
-unzip(zipfile = data.file,exdir = data.path)
+
---
+++ setup.R
@@ -0,0 +1,46 @@
+# Get config.yml parameters
+if (!require("config")) install.packages("config")
+config <- config::get()
+
+
+# Install global project packages not yet installed then load
+global.pkgs <- config$global_pkgs
+
+using<-function(...) {
+  libs<-unlist(list(...))
+  req<-unlist(lapply(libs,require,character.only=TRUE))
+  need<-libs[req==FALSE]
+  if(length(need)>0){ 
+    install.packages(need)
+    lapply(need,require,character.only=TRUE)
+  }
+}
+
+using(global.pkgs)
+
+# Setup project directories
+rawdata.path <- file.path(".",config$data_folder,config$raw_data_subfolder)
+processed_data.path <- file.path(".",config$data_folder,
+                                 config$processed_data_subfolder)
+output.path <- file.path(".",config$output_folder)
+
+paths <- c(rawdata.path,processed_data.path,output.path)
+
+for (dir in paths) {
+  if(!dir.exists(dir)) {dir.create(dir,recursive = TRUE)}
+}
+
+# Clear output folder
+if(config$clear_output) {
+  # get all files in the directories, recursively
+  f <- list.files(output.path, include.dirs = F, full.names = T, recursive = T)
+  # remove the files
+  file.remove(f)
+}
+
+# URL to raw data
+data.url <- config$data_url
+
+#datafile
+data.filename <- basename(data_url)
+data.file <- file.path(data.path,data.filename)
